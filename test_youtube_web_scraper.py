83723ff31f25a2140eaa41d42b31d4f58f0f5129
Alisha <aliasn456@gmail.com>
2024-08-31 23:07:53 -0400

    working scrapper and LLM

--- test_youtube_web_scraper.py
+++ test_youtube_web_scraper.py
@@ -1,30 +1,30 @@
 import re
+import csv
+from datetime import datetime
 from playwright.sync_api import Page, expect
+
 
 def test_has_title(page: Page):
     page.set_viewport_size({"width": 1920, "height": 2080})
     page.goto("https://www.youtube.com/watch?v=x8VkB8ap_FQ") # Put link the youtube video 
 
     # Expect a title "to contain" a substring.
-    page.get_by_role("heading", name="Sabrina Carpenter - Bed Chem (Official Lyric Video)").click()
-    page.wait_for_timeout(30000)
-    # page.keyboard.down("ControlOrMeta")
-    # page.keyboard.press("+")
-    # page.keyboard.press("+")
-    # page.keyboard.press("+")
-    # page.keyboard.press("+")
+    page.get_by_role("heading", name="Sabrina Carpenter - Bed Chem (Official Lyric Video)", exact=True).click()
     page.locator("#leading-section").get_by_text("Comments").click()
-    inner_text = page.locator("#main").all_text_contents()
+    inner_comments = page.locator("#main").all()
 
-    for index in range(inner_text):
-        inner_text[index] = re.sub(r'a', 'b', inner_text[index])
-    assert inner_text == ['her lyrics are so unserious']
-
-# def test_get_started_link(page: Page):
-#     page.goto("https://playwright.dev/")
-
-#     # Click the get started link.
-#     page.get_by_role("link", name="Get started").click()
-
-#     # Expects page to have a heading with the name of Installation.
-#     expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+    # datetime object containing current date and time
+    now = datetime.now()
+    
+    print("now =", now)
+    
+    with open('comments-{}.csv'.format(now), 'x') as csvfile:
+        # creating a csv writer object
+        csvwriter = csv.writer(csvfile)
+        # writing the fields
+        csvwriter.writerow(['author','comment'])
+        # writing the data rows
+        for index in range(15):
+            author_name = re.sub('\n.*','',inner_comments[index].locator("#header-author").inner_text())
+            inner = re.sub('"','',inner_comments[index].locator("#content-text").inner_text())
+            csvwriter.writerow([author_name, inner])
